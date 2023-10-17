f1208d3b9437e5ce284281bca74ede9d02dfa26f
Morgan Mastrangelo <mmastrangelo1120@gmail.com>
2023-10-17 19:47:06 +0300

    Added interface

--- VideoFaceRecognition.py
+++ VideoFaceRecognition.py
@@ -14,22 +14,99 @@ class VideoFaceRecognition:
         self.__knownFaceNames = []
 
         self.__processThisFrame = True
+        self.__repeatVideo = False
+        self.__smallFrameScale = 0.5
+        self.__faceRecognitionTolerance = 0.8
+        self.__outputFrameScale = 1.0
+        self.__faceRectColor = (255, 0, 0)
+        self.__faceNameColor = (255, 255, 255)
 
         self.__prepareFaceImages()
         self.__videoTitle = Path(self.__pathToVideo).stem
 
-    def recognizeAndShow(self):
+    @property
+    def repeatVideo(self) -> float:
+        return self.__repeatVideo
+
+    @repeatVideo.setter
+    def repeatVideo(self, value) -> None:
+        if (type(value) is not bool):
+            raise ValueError("The value must be 'True' or 'False'")
+
+        self__repeatVideo = value
+
+    @property
+    def smallFrameScale(self) -> float:
+        return self.__smallFrameScale
+
+    @smallFrameScale.setter
+    def smallFrameScale(self, value: float) -> None:
+        if (value <= 0.0 or value > 1.0):
+            raise ValueError("The value must be in range (0.0, 1.0]")
+
+        self.__smallFrameScale = value
+
+    @property
+    def faceRecognitionTolerance(self) -> float:
+        return self.__faceRecognitionTolerance
+
+    @faceRecognitionTolerance.setter
+    def faceRecognitionTolerance(self, value: float) -> None:
+        if (value <= 0.0 or value > 1.0):
+            raise ValueError("The value must be in range (0.0, 1.0]")
+
+        self.__faceRecognitionTolerance = value
+
+    @property
+    def outputFrameScale(self) -> float:
+        return self.__outputFrameScale
+
+    @outputFrameScale.setter
+    def outputFrameScale(self, value: float) -> None:
+        if (value <= 0.0 or value > 1.0):
+            raise ValueError("The value must be in range (0.0, 1.0]")
+
+        self.__outputFrameScale = value
+
+    @property
+    def faceRectColor(self) -> tuple:
+        return self.__faceRectColor
+
+    @faceRectColor.setter
+    def faceRectColor(self, color: tuple) -> None:
+        if (not color or len(color) < 3):
+            raise ValueError("The value must be, for example, (134, 255, 11)")
+
+        self.__faceRectColor = self.__convertBGR2RGB(color)
+
+    @property
+    def faceNameColor(self) -> tuple:
+        return self.__faceNameColor
+
+    @faceNameColor.setter
+    def faceNameColor(self, color: tuple) -> None:
+        if (not color or len(color) < 3):
+            raise ValueError("The value must be, for example, (134, 255, 11)")
+
+        self.__faceNameColor = self.__convertBGR2RGB(color)
+
+    def recognizeAndShow(self) -> None:
         video = cv2.VideoCapture(self.__pathToVideo)
     
         while True:
             ret, frame = video.read()
 
-            if not ret:
+            if not ret and self.__repeatVideo:
                 video.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 continue
 
             if self.__processThisFrame:
-                smallFrame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                smallFrame = cv2.resize(
+                    frame, 
+                    (0, 0), 
+                    fx=self.__smallFrameScale, 
+                    fy=self.__smallFrameScale
+                )
                 rgbSmallFrame = cv2.cvtColor(smallFrame, cv2.COLOR_BGR2RGB)
 
                 faceLocations = frn.face_locations(rgbSmallFrame)
@@ -44,7 +121,7 @@ class VideoFaceRecognition:
                     matches = frn.compare_faces(
                         self.__knownFaceEncodings,
                         fe,
-                        tolerance=0.8
+                        tolerance=self.__faceRecognitionTolerance
                     )
 
                     name = None
@@ -68,16 +145,15 @@ class VideoFaceRecognition:
                 if not name:
                     continue
 
-                top *= 2
-                right *= 2
-                bottom *= 2
-                left *= 2
+                top, right, bottom, left = self.__scaleFaceLocation(
+                    top, right, bottom, left
+                )
 
                 cv2.rectangle(
                     frame, 
                     (left, top), 
                     (right, bottom), 
-                    (0, 0, 255), 
+                    self.__faceRectColor, 
                     2
                 )
                 font = cv2.FONT_HERSHEY_DUPLEX
@@ -87,11 +163,16 @@ class VideoFaceRecognition:
                     (left, bottom + 30), 
                     font, 
                     1.0, 
-                    (255, 255, 255), 
+                    self.__faceNameColor,
                     2
                 )
 
-            resizedFrame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+            resizedFrame = cv2.resize(
+                frame, 
+                (0, 0), 
+                fx=self.__outputFrameScale, 
+                fy=self.__outputFrameScale
+            )
             cv2.imshow(self.__videoTitle, resizedFrame)
 
             key = cv2.waitKey(1)
@@ -102,7 +183,10 @@ class VideoFaceRecognition:
         video.release()
         cv2.destroyAllWindows()
 
-    def __prepareFaceImages(self):
+    def __prepareFaceImages(self) -> None:
+        if not self.__pathToFaces:
+            return 
+
         for path in self.__pathToFaces:
             img = frn.load_image_file(path)
             self.__faceImages.append(img)
@@ -112,3 +196,16 @@ class VideoFaceRecognition:
 
             fileName = Path(path).stem
             self.__knownFaceNames.append(fileName)
+
+    def __scaleFaceLocation(self, top, right, bottom, left) -> list:
+        scaleMultiplier = int(1 / self.__smallFrameScale)
+
+        top    *= scaleMultiplier 
+        right  *= scaleMultiplier 
+        bottom *= scaleMultiplier 
+        left   *= scaleMultiplier 
+
+        return top, right, bottom, left
+
+    def __convertBGR2RGB(self, color: tuple) -> tuple:
+        return (color[2], color[1], color[0])
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
+from tkinter.colorchooser import askcolor
 import VideoFaceRecognition as VFR
 
 root = Tk()
@@ -10,7 +11,15 @@ root.geometry("300x200")
 def openVideoFile():
     global videoFilePath
 
-    pathToVideo = filedialog.askopenfilename()
+    pathToVideo = filedialog.askopenfilename(
+        title="Select video file",
+        filetypes=[
+            ("Video", ".mp4 .avi"),
+            ("MP4", ".mp4"),
+            ("AVI", ".avi")
+        ],
+        multiple=False
+    )
 
     if pathToVideo != "":
         videoFilePath = pathToVideo
@@ -18,22 +27,80 @@ def openVideoFile():
 def openFaceImages():
     global facesFilePath
 
-    pathToFaces = filedialog.askopenfilenames()
+    pathToFaces = filedialog.askopenfilenames(
+        title="Select face images",
+        filetypes=[
+            ("Images", ".png .jpg .jpeg .webp"),
+            ("PNG", ".png"),
+            ("JPG", ".jpg"),
+            ("JPEG", ".jpeg"),
+            ("WEBP", ".webp")
+        ],
+        multiple=True
+    )
 
     if pathToFaces != "":
         facesFilePath = pathToFaces
 
+def setFaceRectColor():
+    global faceRectColor
+
+    color = askcolor(title="Select face rectangle color")
+
+    if color is not None:
+        faceRectColor = color[0]
+
+def setFaceNameColor():
+    global faceNameColor
+
+    color = askcolor(title="Select face name color")
+
+    if color is not None:
+        faceNameColor = color[0]
+
 def recognizeAndShow():
     vfr = VFR.VideoFaceRecognition(videoFilePath, facesFilePath)
+    
+    try:
+        vfr.faceRectColor = faceRectColor
+    except:
+        pass
+    
+    try:
+        vfr.faceNameColor = faceNameColor
+    except:
+        pass
+
     vfr.recognizeAndShow()
 
-openVideoBtn = ttk.Button(text="Open Video", command=openVideoFile)
-openVideoBtn.pack(fill=X)
+openVideoBtn = ttk.Button(
+    text="Open Video", 
+    command=openVideoFile
+).pack(fill=X)
 
-loadFacesBtn = ttk.Button(text="Load Faces", command=openFaceImages)
-loadFacesBtn.pack(fill=X)
+loadFacesBtn = ttk.Button(
+    text="Load Faces", 
+    command=openFaceImages
+).pack(fill=X)
 
-recAndShowBtn = ttk.Button(text="Execute", command=recognizeAndShow)
-recAndShowBtn.pack(fill=X)
+recAndShowBtn = ttk.Button(
+    text="Execute", 
+    command=recognizeAndShow
+).pack(fill=X)
+
+horizontalSeparator = ttk.Separator(
+    master=root,
+    orient=HORIZONTAL
+).pack(fill=X, pady=10,)
+
+rectColorChooserBtn = ttk.Button(
+    text="Set Face Rect Color", 
+    command=setFaceRectColor
+).pack(fill=X)
+
+faceNameColorBtn = ttk.Button(
+    text="Set Face Name Color", 
+    command=setFaceNameColor
+).pack(fill=X)
 
 root.mainloop()
