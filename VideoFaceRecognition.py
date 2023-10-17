77b57006b04d31ebb5e6df5848d5cfa2e8176859
Morgan Mastrangelo <mmastrangelo1120@gmail.com>
2023-10-17 16:19:24 +0300

    Initial commit

---
+++ VideoFaceRecognition.py
@@ -0,0 +1,8 @@
+import face_recognition as frn
+import numpy as np
+import cv2
+
+class VideoFaceRecognition:
+
+    def __init__(self):
+        pass
---
+++ app.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root = Tk()
+root.title("Face Recognition")
+root.geometry("800x600")
+
+root.mainloop()
